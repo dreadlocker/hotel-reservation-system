bbe27c6e66d990c2cfbaf98eaf816ece66b64f19
Svetozar <svetozariliev13@gmail.com>
2020-06-01 19:04:32 +0300

    Show date of booking

---
+++ src/views/Reservations/Reservation.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h3>{{date}}</h3>
+    <RoomBooked
+      v-for="(count, roomType) in reservation"
+      :key="roomType"
+      :count="count"
+      :roomType="roomType"
+    />
+  </div>
+</template>
+
+<script>
+import RoomBooked from './RoomBooked.vue';
+
+export default {
+  name: 'Reservation',
+  components: {
+    RoomBooked,
+  },
+  props: {
+    reservation: {
+      type: Object,
+      required: true,
+    },
+    time: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    date() {
+      const date = new Date(Number(this.time))
+      const day = date.getDate()
+      const month = date.toLocaleString('default', { month: 'long' });
+      const year = date.getFullYear()      
+      return `${day} ${month} ${year}`
+    },
+  }
+}
+</script>
