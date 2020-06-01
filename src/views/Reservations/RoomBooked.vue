c6f2cceac125938e6b8464400627328eb3b07e81
Svetozar <svetozariliev13@gmail.com>
2020-06-01 19:03:38 +0300

    Show type of room that is booked and how many of them, for the current day

---
+++ src/views/Reservations/RoomBooked.vue
@@ -0,0 +1,19 @@
+<template>
+  <div><span>{{roomType}} - </span><span>{{count}}</span></div>
+</template>
+
+<script>
+export default {
+  name: 'RoomBooked',
+  props: {
+    count: {
+      type: String,
+      required: true,
+    },
+    roomType: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
