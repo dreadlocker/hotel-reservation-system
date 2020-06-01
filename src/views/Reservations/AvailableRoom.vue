3fde8d05b0f3443f2a89ab7477f59659a784f277
Svetozar <svetozariliev13@gmail.com>
2020-06-01 23:07:49 +0300

    Replaced a component with 2 other components

---
+++ src/views/Reservations/AvailableRoom.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h3>Available rooms:</h3>
+    
+    <div
+      v-for="(roomCount, roomType) in availableDays"
+      :key="roomType"
+    ><span>{{roomType}} - </span><span>{{roomCount}}</span></div>
+    <hr>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AvailableRoom',
+  props: {
+    availableDays: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
---
+++ src/views/Reservations/BookedRooms.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h3>Booked rooms:</h3>
+
+    <div
+      v-for="(roomCount, roomType) in bookedDays"
+      :key="roomType"
+    ><span>{{roomType}} - </span><span>{{roomCount}}</span></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BookedRooms',
+  props: {
+    bookedDays: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
