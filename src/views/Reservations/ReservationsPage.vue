a30434d2b730bbae14f5ccef340b8da38ce5852c
Svetozar <svetozariliev13@gmail.com>
2020-06-01 19:05:25 +0300

    Logic for ReservationsPage finished

--- src/views/Reservations/ReservationsPage.vue
+++ src/views/Reservations/ReservationsPage.vue
@@ -1,11 +1,25 @@
 <template>
   <div>
-    Reservations
+    <Reservation
+      v-for="(reservation, time) in reservationsSorted"
+      :key="time"
+      :reservation="reservation"
+      :time="time"
+    />
   </div>
 </template>
 
 <script>
+import {mapGetters} from "vuex";
+import Reservation from './Reservation.vue';
+
 export default {
   name: 'ReservationsPage',
+  components: {
+    Reservation,
+  },
+  computed: {
+    ...mapGetters(["reservationsSorted"]),
+  }
 }
 </script>
