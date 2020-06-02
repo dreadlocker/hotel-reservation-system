e2376ee195c97e29cf1fcdd638e6b0419e7683a4
Svetozar <svetozariliev13@gmail.com>
2020-06-02 16:23:40 +0300

    Created BaseButton component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <button
+    :type="type"
+    :classes="classes"
+    :name="name"
+    class="btn"
+  ><slot></slot></button>
+</template>
+
+<script>
+export default {
+  name: 'BaseButton',
+  props: {
+    type: {
+      type: String,
+      default: "button",
+      validator: (val) => (/^button$|^submit$|^reset$/g).test(val)
+    },
+    classes: {
+      type: String,
+    },
+    name: {
+      type: String,
+      validator: (val) => (/^[A-Za-z0-9]{1,30}$/g).test(val)
+    },
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.btn
+  padding: 5px 10px
+  &:active
+    transform: scale(1.1)
+</style>
