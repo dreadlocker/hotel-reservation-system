025d0fb7d6a9b866165c088a4f23281c05fd50fd
Svetozar <svetozariliev13@gmail.com>
2020-06-02 10:54:51 +0300

    Use array instead of object, changed getters names, added new getter

--- src/store/index.js
+++ src/store/index.js
@@ -8,8 +8,8 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   plugins: [createPersistedState()],
   state: {
-    roomsInfo: {},
-    reservations: {},
+    roomsInfo: [],
+    reservations: [],
   },
   mutations: {
     [types.ROOMS_INFO]: (state, payload) => {
@@ -29,18 +29,40 @@ export default new Vuex.Store({
   },
   getters: {
     reservationsSorted: state => {
-      const reservationsKeysSorted = (Object.keys(state.reservations).sort((a, b) => a - b));
-      const reservationsSorted = []
-      reservationsKeysSorted.forEach(time => {
-        reservationsSorted.push({[time]: state.reservations[time]})
-      });
-      return reservationsSorted;
+      const reservationsKeysSorted = state.reservations.map(reservation => Object.keys(reservation)[0]).sort((a, b) => Number(a) - Number(b));
+      return reservationsKeysSorted.map(time => state.reservations.find(reservation => Object.keys(reservation)[0] === time));
     },
-    bookedDays: (state, getters) => {
+    bookedRooms: (state, getters) => {
       return getters.reservationsSorted.map(dayInfo => Object.values(dayInfo)[0].booked);
     },
-    availableDays: (state, getters) => {
+    availableRooms: (state, getters) => {
       return getters.reservationsSorted.map(dayInfo => Object.values(dayInfo)[0].available);
+    },
+    roomsInfoBySeason: state => {
+      const roomsInfo = []
+      const stateRoomsInfo = state.roomsInfo
+      const monthIndex = new Date().getMonth();
+      // promotional price is set for months June, July, August
+      const isSummer = monthIndex > 4 && monthIndex < 8
+
+      return stateRoomsInfo.map(room => {
+        const roomType = Object.keys(room)[0]
+        const sortedRoomInfo = {}
+        const roomInfo = Object.values(room)[0]
+        for (const info in roomInfo) {
+          if (isSummer) {
+            if(info === "discountedPrice") continue
+            if(info === "price") {
+              sortedRoomInfo["price"] = roomInfo["discountedPrice"]
+              continue
+            }
+            sortedRoomInfo[info] = roomInfo[info]
+          } else {
+            if(info !== "discountedPrice") sortedRoomInfo[info] = roomInfo[info]
+          }
+        }
+        return {[roomType]: sortedRoomInfo}
+      })
     },
   }
 })
